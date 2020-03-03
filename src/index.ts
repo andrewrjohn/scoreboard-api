cfea86af73a82eef8297055128c6e72f5ab602ca
Andrew <ajlutheran@gmail.com>
2020-03-02 18:55:48 -0600

    Changed port to not conflict with FE

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const express = require("express");
 const scoreboardRoute = require("./routes/scoreboard");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use("/api/v1/sports", [scoreboardRoute]);
 
